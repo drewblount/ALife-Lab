59fefba183127882cb79d5a21bd5b1c0f0981fdb
Drew Blount <dblount@reed.edu>
2014-07-07 16:07:44 -0700

    add top n words selector, text sorter, and random patent selector

---
+++ Cultural Evolution/patDB_explorer/randPat.py
@@ -0,0 +1,62 @@
+from pymongo  import MongoClient, ASCENDING, DESCENDING
+from bson.code import Code
+import random
+
+patDB = MongoClient().patents
+patns = patDB.patns
+
+patns.ensure_index('pno')
+
+minPno = patDB
+
+# a random patent selector. 'projection' is a mongodb projection
+# which describes which fields to return; if left {} the entire
+# patent will be returned.
+class Selector(object):
+	
+	def __init__(self, patCol, projection = {}, randSeed = None, db = patDB):
+	
+		print 'gettin max'
+		self.maxPno = patCol.find({},{'pno': 1, '_id': 0}).sort('pno',DESCENDING)[0]['pno']
+		print 'max is ' + str(self.maxPno)
+
+		print 'gettin min'
+		self.minPno = patCol.find({},{'pno': 1, '_id': 0}).sort('pno',ASCENDING)[0]['pno']
+
+
+		print 'min is ' + str(self.minPno)
+
+
+		patCol.ensure_index('pno', ASCENDING)
+		
+		
+		# default randSeed is system time
+		random.seed(randSeed)
+
+		self.col = patCol
+		self.proj = projection
+
+
+	# randomly chooses a patent number in the pno range covered by
+	# the patent collection
+	# Set retryIfAbsent to False if the collection contains relatively
+	# few pnos between minPno and maxPno; this results in a non-uniform
+	# random distribution
+	def randPat(self, retryIfAbsent=True):
+		randPno = random.randint(self.minPno, self.maxPno)
+		print 'rand pno is ' + str(randPno)
+		if retryIfAbsent:
+			while not self.col.find_one( {'pno' : randPno}, {'pno': 1, '_id': 0}):
+				randPno = random.randint(self.minPno, self.maxPno)
+			return self.col.find_one( {'pno' : randPno}, self.proj )
+		else:
+			return self.col.find_one( {'pno' : {'$gte': randPno} }, self.proj )
+
+	# in case that patent num is not in the collection, this finds
+	# the number that is closest above it
+
+
+
+
+
+
---
+++ Cultural Evolution/patDB_explorer/topWords.py
@@ -0,0 +1,102 @@
+# Goal: order the words in a patent's text field by tf-idf (descending)
+
+# python treats dictionaries as unordered, and it's awkward to reorder
+# MongoDB's BSON objects, so I use a python array of dicts to hold the text
+# while I shuffle it around
+
+from pymongo  import MongoClient
+from operator import attrgetter
+
+
+# for operations where a document's order is important,
+# we want to store mongo objects as SON and not python
+# dicts (which do not preserve order)
+
+
+patDB = MongoClient().patents
+patns = patDB.patns
+
+
+# copies {key1: v1, key2: v2} into [{key: key1, v1}, {key: key2, v2}]
+# (so there is some flattening, convenient for our use)
+def dictToArray(D, keylabel = "key"):
+	arr = []
+	for elem in D:
+		# just copies the dict entry in an array
+		newElem = D[elem]
+		newElem[keylabel] = elem
+		arr.append(newElem)
+	return arr
+
+# For a patent, produces an array of the words in that patent's text
+# in descending tf-idf order
+def createSortedText(patn):
+	textArray = dictToArray(patn['text'], keylabel='word')
+	sortedTextArray = sorted(textArray, key=lambda w: w['tf-idf'], reverse = True)
+	return sortedTextArray
+
+
+def perLineArrayDisplay(arr):
+	for elem in arr:
+		print elem
+
+# pretty prints a word and its tf-idf from a patent's sorted_text
+def displaySortedWord(word):
+	# makes all words width-20
+	pstring = '{0: <15}'.format(word['word']) + ' tf-idf: ' + "%.3f" % word['tf-idf']
+	print pstring
+
+
+def topNTerms(patn, n, patCol_to_update = False, display=False):
+	if 'sorted_text' not in patn:
+		patn['sorted_text'] = createSortedText(patn)
+		# if the optional patCol_to_update arg is used, and the patent's text has never
+		# been sorted before, save the sorted text in the patent collection
+		if patCol_to_update:
+			patCol_to_update.update({'pno': patn['pno']},{'$set': {'sorted_text': patn['sorted_text']}})
+	if display:
+		print '\n'
+		print 'Patent ' + str(pat['pno']) + ': ' + pat['title']
+		for word in patn['sorted_text'][:n]:
+			displaySortedWord(word)
+	return patn['sorted_text'][:n]
+
+
+def orderAllTexts(disp= False, showN= 10):
+	pats = patns.find({}, {'pno':1, 'title': 1, 'text': 1, 'sorted_text': 1})
+	for pat in pats:
+		topNTerms(pat, showN, display=disp, patCol_to_update=patns)
+
+
+# Returns the number of words shared by the top n words in each patent
+# if returnWords is true, returns a list of the shared words. Otherwise
+# just returns the number of shared words.
+def sharedTopN(pat1, pat2, n, returnWords=False, patCol_to_update = False):
+	words1, words2 = topNTerms(pat1, n, patCol_to_update), topNTerms(pat2, n, patCol_to_update)
+	shCount = 0
+	shWords = []
+	for word1 in words1:
+		for word2 in words2:
+			if word1['word'] == word2['word']:
+				if returnWords: shWords.append(word1['word'])
+				else: shCount += 1
+				break
+	if returnWords: return shWords
+	else: return shCount
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
