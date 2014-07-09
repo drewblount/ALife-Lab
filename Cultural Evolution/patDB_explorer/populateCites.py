a62c681aab505a8ebbc657f534f81502dc6bfee7
Drew Blount <dblount@reed.edu>
2014-07-09 11:14:55 -0700

    limit batch size, prevent duplicate adding

--- Cultural Evolution/patDB_explorer/populateCites.py
+++ Cultural Evolution/patDB_explorer/populateCites.py
@@ -18,16 +18,29 @@ landmarkPno = 4000000
 # hopefully allows parallelization
 bulk = client[dbname]['patns'].initialize_unordered_bulk_op()
 
+addToSetUntil = 4400000
+
+patents.ensure_index('pno')
 
 # Load only the pno and rawcites from each patent, sort by pno so progress reports are possible
-for citingPatn in patents.find( {}, {'rawcites':1, 'pno':1} ).sort( [ ('pno' , 1) ] ):
-	if citingPatn['pno'] >= landmarkPno:
+# limiting batch_size to keep memory down, reduce timeouts
+for citingPatn in patents.find( {}, {'rawcites':1, 'pno':1} ).sort( [ ('pno' , 1) ] ).batch_size(100000):
+	citingNo = citingPatn['pno']
+	if citingNo >= landmarkPno:
 		print 'drawing back-citations for patn ' + str (landmarkPno)
 		landmarkPno += 100000
 		bulk.execute()
 		bulk = client[dbname]['patns'].initialize_unordered_bulk_op()
-	citingNo = citingPatn['pno']
-	for citedPNo in citingPatn['rawcites'] :
-		# 'bulk' will (in theory) execute all of these update_ones in parallel, in nondeterministic order
-		bulk.find( {'pno' : citedPNo} ).update_one( {'$push' : {'citedby': citingNo} } )
+
+	if citingNo < addToSetUntil:
+		for citedNo in citingPatn['rawcites'] :
+			# addToSet makes sure not to add duplicates
+			bulk.find( {'pno' : citedNo} ).update_one( {'$addToSet' : {'citedby': citingNo} } )
+	else:
+		for citedPNo in citingPatn['rawcites'] :
+			# 'bulk' will (in theory) execute all of these update_ones in parallel, in nondeterministic order
+			bulk.find( {'pno' : citedNo} ).update_one( {'$push' : {'citedby': citingNo} } )
+
+	bulk.find( {'pno' : citingNo} ).update_one( {'$set': {'backCitesDrawn' = True} })
+
 bulk.execute()
