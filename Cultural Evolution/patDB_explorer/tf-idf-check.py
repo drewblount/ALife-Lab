fba4c5d1ec4e9ce87b8b5a8ae4ca83415ecec7a1
Drew Blount <dblount@reed.edu>
2014-07-16 00:06:42 -0700

    adds tf-idf-check

---
+++ Cultural Evolution/patDB_explorer/tf-idf-check.py
@@ -0,0 +1,28 @@
+
+from pymongo import MongoClient
+from parallelMap import parallelMap
+
+patDB = MongoClient().patents
+patns = .patns
+
+
+def has_tf_idfs(pat):
+	if 'text' not in pat: return False
+	for word in pat['text']:
+		if 'tf-idf' in word: return True
+		else: return False
+
+def return_updated_text(patn):
+	if has_tf_idfs(patn):
+		return
+	else:
+	for word in patn['text']:
+		idf = patDB.corpusDict.find_one({'_id': word})['value']['idf']
+		patn['text'][word]['tf-idf'] = patn['text'][word]['tf'] * idf
+	return( {'$set': {'text' : patn['text'] } } )
+
+def check_tf_idfs(pats):
+	parallelMap(tf_idf_onePat, patDB.patns,
+				findArgs = {'spec': {}, 'fields': {'text': 1} },
+				updateFreq = 5000,
+				bSize = 5000)
