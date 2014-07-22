4335e5e606e91b4edaf73b041e29cde40dbf8c4f
Drew Blount <dblount@reed.edu>
2014-07-21 23:27:59 -0700

    adds nonworking (bc each proc's random seed is the same) parallel_bulk_tf_idfs_by_shared

--- Cultural Evolution/patDB_explorer/csv_module.py
+++ Cultural Evolution/patDB_explorer/csv_module.py
@@ -12,13 +12,13 @@ glob_min = metadata.find_one({'_id':'min_pno'})['val']
 
 def save_csv(value_array, out_name):
 	# using the 'a' tag means that if the file already exists, it is appended to
-	outf = open(out_name + '.csv', 'a')
+	outf = open(out_name + '.csv', 'a+')
 	outf.write(','.join( map(str,value_array) ) )
 	outf.close()
 
 def save_csvs(list_of_value_arrays, out_name):
 	for i in range( len(list_of_value_arrays) ):
-		outf = open(out_name + '.' + str(i+1) + '.csv', 'a')
+		outf = open(out_name + '.' + str(i+1) + '.csv', 'a+')
 		outf.write(','.join( map(str, list_of_value_arrays[i]) ) )
 		outf.close()
 
