9337fec969c583d99d9f81f558453eecf10eaa38
Drew Blount <dblount@reed.edu>
2014-08-07 00:56:41 -0700

    script for saving shared top-n vectors

---
+++ Cultural Evolution/patDB_explorer/aug7script.py
@@ -0,0 +1,20 @@
+execfile('topWords.py')
+
+maxruns = 100000
+
+dumpfreq = maxruns / 100
+
+n = 25
+
+
+# sum all of the columns in output to get final result
+for i in range(100):
+
+	outC = shared_n_vectors(n, maxruns, cite_pairs=True, texts_already_ordered=False)
+
+	csv_module.save_csv(outC, 'cited_shared_vects_%d_trials_%_perrow' % (maxruns,dumpfreq)
+
+	outC = shared_n_vectors(n, maxruns, cite_pairs=False, texts_already_ordered=False)
+	
+	csv_module.save_csv(outC, 'rand_shared_vects_%d_trials_%_perrow' % (maxruns,dumpfreq)
+
