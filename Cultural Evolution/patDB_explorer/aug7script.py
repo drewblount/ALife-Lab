dee9147bdf103a04a6dfdb547fb2a963e20eeb45
Drew Blount <dblount@reed.edu>
2014-08-07 13:21:54 -0700

    fixes rand output

--- Cultural Evolution/patDB_explorer/aug7script.py
+++ Cultural Evolution/patDB_explorer/aug7script.py
@@ -10,11 +10,11 @@ n = 25
 # sum all of the columns in output to get final result
 for i in range(100):
 
-	outC = shared_n_vectors(n, maxruns, cite_pairs=True, texts_already_ordered=False)
+	#outC = shared_n_vectors(n, maxruns, cite_pairs=True, texts_already_ordered=False)
 
-	csv_module.save_csv(outC, 'cited_shared_vects_%d_trials_%d_perrow' % (maxruns,dumpfreq) )
+	#csv_module.save_csv(outC, 'cited_shared_vects_%d_trials_%d_perrow' % (maxruns,dumpfreq) )
 
 	outR = shared_n_vectors(n, maxruns, cite_pairs=False, texts_already_ordered=False)
 	
-	csv_module.save_csv(outC, 'rand_shared_vects_%d_trials_%d_perrow' % (maxruns,dumpfreq) )
+	csv_module.save_csv(outR, 'rand_shared_vects_%d_trials_%d_perrow' % (maxruns,dumpfreq) )
 
