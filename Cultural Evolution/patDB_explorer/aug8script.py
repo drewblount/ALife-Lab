a020b6c8a2fdfc0374702e7473e5ac0ffacfde19
Drew Blount <dblount@reed.edu>
2014-08-11 13:37:15 -0700

    script update, modular functionality

--- Cultural Evolution/patDB_explorer/aug8script.py
+++ Cultural Evolution/patDB_explorer/aug8script.py
@@ -38,20 +38,27 @@ plt.show()
 
 import csv
 out = []
-with open('sh_term_ranks_cite-pairs_topn=13_num=100000.csv', 'rb') as csvf:
+num_trials = 1000
+with open('sh_term_ranks_cite-pairs_topn=13_num=%d.csv' % num_trials, 'rb') as csvf:
 	reader = csv.reader(csvf, delimiter=',')
 	for row in reader:
 		out.append(row)
 
-p1s = [int(out[i][2]) for i in range(1,100001)]
-p2s = [int(out[i][3]) for i in range(1,100001)]
+p1s = [int(out[i][2]) for i in range(1,num_trials+1)]
+p2s = [int(out[i][3]) for i in range(1,num_trials+1)]
+
+cite_pairs = True
 
 from numpy import mean, std
 p1m, p1std, p2m, p2std = mean(p1s), std(p1s), mean(p2s), std(p2s)
+p1label, p2label, pairlabel = (('children','parents','cite')
+							   if cite_pairs else
+							   ('pat 1', 'pat 2', 'rand'))
+
 
 from matplotlib import pyplot as plt
-plt.hist(p1s, alpha=0.5,bins=13, label = 'children')
-plt.hist(p2s, alpha=0.5,bins=13, label = 'parents')
+plt.hist(p1s, alpha=0.5,bins=13, label=p1label, normed=True)
+plt.hist(p2s, alpha=0.5,bins=13, label=p2label, normed=True)
 plt.legend(loc='upper right')
-plt.title('cite pairs, tf-idf ranks of shared terms; 100,000 shared terms')
+plt.title('%s pairs, tf-idf ranks of %d shared terms' % (pairlabel, num_trials))
 plt.show()
