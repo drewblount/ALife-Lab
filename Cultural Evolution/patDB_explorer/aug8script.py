3416a396c24e5b6f7a7f5a89a21da55149990e50
Drew Blount <dblount@reed.edu>
2014-08-12 13:45:33 -0700

    adds more pastable scripts

--- Cultural Evolution/patDB_explorer/aug8script.py
+++ Cultural Evolution/patDB_explorer/aug8script.py
@@ -38,27 +38,96 @@ plt.show()
 
 import csv
 out = []
-num_trials = 1000
-with open('sh_term_ranks_cite-pairs_topn=13_num=%d.csv' % num_trials, 'rb') as csvf:
+num_trials = 100000
+with open('sh_term_ranks_rand-pairs_topn=13_num=%d.csv' % num_trials, 'rb') as csvf:
 	reader = csv.reader(csvf, delimiter=',')
 	for row in reader:
 		out.append(row)
 
-p1s = [int(out[i][2]) for i in range(1,num_trials+1)]
-p2s = [int(out[i][3]) for i in range(1,num_trials+1)]
+p1s = [int(out[i][2]) for i in range(1,len(out))]
+p2s = [int(out[i][3]) for i in range(1,len(out))]
 
 cite_pairs = True
+normed = True
 
 from numpy import mean, std
 p1m, p1std, p2m, p2std = mean(p1s), std(p1s), mean(p2s), std(p2s)
 p1label, p2label, pairlabel = (('children','parents','cite')
 							   if cite_pairs else
 							   ('pat 1', 'pat 2', 'rand'))
+normlabel = ' (normed)' if normed else ''
 
 
 from matplotlib import pyplot as plt
-plt.hist(p1s, alpha=0.5,bins=13, label=p1label, normed=True)
-plt.hist(p2s, alpha=0.5,bins=13, label=p2label, normed=True)
-plt.legend(loc='upper right')
-plt.title('%s pairs, tf-idf ranks of %d shared terms' % (pairlabel, num_trials))
+plt.hist(p1s, alpha=0.5,bins=13, label=p1label, normed=normed)
+plt.hist(p2s, alpha=0.5,bins=13, label=p2label, normed=normed)
+plt.legend(loc='upper left')
+plt.title('%s pairs, tf-idf ranks of %d shared terms%s' % (pairlabel, num_trials,normlabel))
 plt.show()
+
+
+
+
+
+
+
+
+# for getting slopes from the files in the figures/sh_term_ranks folder
+import csv
+out = []
+num_trials = 100000
+top_n = 13
+normed=True
+with open('rand-pairs_topn=%d_num=%d.csv' % (top_n, num_trials), 'rb') as csvf:
+	reader = csv.reader(csvf, delimiter=',')
+	for row in reader:
+		out.append(row)
+
+slopes = [int(out[i][3])-int(out[i][2]) for i in range(1,len(out))]
+bins = [i for i in range(1-top_n, top_n+1)]
+from matplotlib import pyplot as plt
+plt.hist(slopes, bins, normed=normed)
+norm_label = '(output normed)' if normed else ''
+plt.title('slope between shared term ranks\n%d random pairs %s' % (num_trials,norm_label))
+plt.show()
+
+
+
+
+# for histogramming the rank distribution data
+import csv
+out = []
+num_trials = 10000
+top_n = 13
+normed=True
+with open('rank_dist_random_pats_topn=13_num=10000.csv' % (top_n, num_trials), 'rb') as csvf:
+	reader = csv.reader(csvf, delimiter=',')
+	for row in reader:
+		out.append(row)
+
+bins = [i for i in range(top_n)]
+from matplotlib import pyplot as plt
+plt.hist(out, bins, normed=normed)
+norm_label = '(output normed)' if normed else ''
+plt.title('terms of rank i in the top %d terms for %d rand patents %s' % (top_n, num_trials, norm_label))
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
