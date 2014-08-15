b21009573e5b073976faac38721e53c6dfed4ec4
Drew Blount <dblount@reed.edu>
2014-08-15 12:10:10 -0700

    makes more command-line-usable

--- Cultural Evolution/patDB_explorer/aug14script.py
+++ Cultural Evolution/patDB_explorer/aug14script.py
@@ -1,8 +1,21 @@
-execfile('topWords.py')
 
-df_by_rank(100000, 13, cite_pairs = True, save_freq = 1000)
-df_by_rank(100000, 13, cite_pairs = False, save_freq= 1000)
+def runScript():
+	execfile('topWords.py')
 
-# just incase somethin's weird with save_freq
-df_by_rank(100000, 13, cite_pairs = True, fname_suffix='backup')
-df_by_rank(100000, 13, cite_pairs = False, fname_suffix='backup')
+	df_by_rank(100000, 13, cite_pairs = True, save_freq = 1000)
+	df_by_rank(100000, 13, cite_pairs = False, save_freq= 1000)
+
+	# just incase somethin's weird with save_freq
+	df_by_rank(100000, 13, cite_pairs = True, fname_suffix='backup')
+	df_by_rank(100000, 13, cite_pairs = False, fname_suffix='backup')
+
+
+import csv
+def fixup(fname):
+	out = []
+	with open(fname, 'rb') as fin:
+		rdr = csv.reader(fin, delimiter=',')
+		for row in rdr:
+			out.append(row)
+	data = [out[j] for j in range(1300,1313)]
+	return data
