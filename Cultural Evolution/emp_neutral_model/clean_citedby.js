3afb8aefbfc394e5936cb7b55064f3bca97a864c
Drew Blount <dblount@reed.edu>
2014-11-05 00:26:25 -0800

    deploy version of clean_citedby, ready-to-deploy update_cites

--- Cultural Evolution/emp_neutral_model/clean_citedby.js
+++ Cultural Evolution/emp_neutral_model/clean_citedby.js
@@ -1,19 +1,6 @@
 // to be applied to each object in cite_net:
 
 function clean(entry) {
-	// 1: deletes duplicates in citedby:
-	var clean_citedby = entry.citedby.filter(function(item, pos) {
-		return myArray.indexOf(item) == pos;
-	});
-	
-	// 2: updates citedby
-	db.cite_net.update( {_id = entry._id}, {citedby: {$set: clean_citedby} } );
-	
-	// 3: copies to patns
-	db.patns.update( {pno = entry._id}, {citedby: {$set: clean_citedby} } );
-}
-
-function clean(entry) {
 	var clean_citedby = entry.citedby.filter(function(item, pos) {
 		return entry.citedby.indexOf(item) == pos;
 	});
