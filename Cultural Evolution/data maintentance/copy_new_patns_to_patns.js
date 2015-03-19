db17ea8c8c5b825f478b4bf6ae96a1807e7f1b64
Drew Blount <dblount@reed.edu>
2015-03-19 00:31:25 -0700

    random debuggin

--- Cultural Evolution/data maintentance/copy_new_patns_to_patns.js
+++ Cultural Evolution/data maintentance/copy_new_patns_to_patns.js
@@ -1,4 +1,4 @@
 // this is a mongo shell script, saved for longevity
 // from http://stackoverflow.com/questions/5681851/mongodb-combine-data-from-multiple-collections-into-one-how
-use patents;
-db.new_patns.find().forEach(function(doc){db.patns.save(doc)});
+use patents
+db.new_patns.find().forEach(function(doc){db.patns.save(doc)})
